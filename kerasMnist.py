677789875d6e132f1ae6523a94e65b4da26f6f63
ke2li <li.kyle25@gmail.com>
2018-08-21 23:27:08 -0400

    Added convolutional neural network. Added loss and accuracy plotting

--- kerasMnist.py
+++ kerasMnist.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
 
@@ -10,14 +11,38 @@ x_train, x_test = x_train/255.0, x_test/255.0
 model = keras.models.Sequential({
 	keras.layers.Flatten(),
 	keras.layers.Dense(512, activation='relu'),
-	#keras.layers.Dense(128, activation='relu'),
+	keras.layers.Dropout(0.25),
+	keras.layers.Dense(128, activation='relu', kernel_regularizer=keras.regularizers.l2(0.01)),
 	keras.layers.Dense(10, activation='softmax')
 })
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs = 5, batch_size=25)
+history = model.fit(x_train, y_train, epochs=50, batch_size=4, validation_data=(x_test, y_test))
 
-test_loss, test_acc = model.evaluate(x_test,y_test, batch_size=25)
+test_loss, test_acc = model.evaluate(x_test,y_test, batch_size=4)
+
+#model.save('C:\\TensorFlow\\nnMnist.h5')
 
 print('Test accuracy:', test_acc)
+
+plt.figure(figsize=[8,6])
+plt.plot(history.history['loss'],'r',linewidth=3.0)
+plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Loss',fontsize=16)
+plt.title('Loss Curves',fontsize=16)
+
+plt.show()
+ 
+# Accuracy Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['acc'],'r',linewidth=3.0)
+plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
+
+plt.show()
